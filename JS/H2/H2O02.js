9faf24c4801bceeead7710d5dea219160b1860eb
poopiemasterxoxo69 <145103241+poopiemasterxoxo69@users.noreply.github.com>
2023-12-15 10:47:29 +0100

    yu

--- JS/H2/H2O02.js
+++ JS/H2/H2O02.js
@@ -2,7 +2,8 @@ var kater,toren,piano;
 
 function preload() {
   kater = loadImage("images/brieck.jpg");
-
+  toren = loadImage("images/toren.jpg")
+  piano = loadImage("images/piano.jpg")
 }
 
 function setup() {
@@ -21,4 +22,6 @@ function setup() {
 function draw() {
   text("afmeting: " + kater.width + " x " + kater.height,30,20);
   image(kater,25,25,250,250);
+  image(toren,300,25,250,250);
+  image(piano,575,25,250,250);
 }
