a0a2db48dc716b91cf919d229a54d7291f54707b
poopiemasterxoxo69 <145103241+poopiemasterxoxo69@users.noreply.github.com>
2024-01-03 15:11:29 +0100

    asd

--- JS/H1/H1O31.js
+++ JS/H1/H1O31.js
@@ -1,16 +1,21 @@
-kleur='white';
+var kleur = 'white';
 
 function setup() {
-  canvas = createCanvas(451,451);
+  canvas = createCanvas(501, 501);
   canvas.parent('processing');
   background('coral');
 }
 
 function draw() {
-  for (var rij = 0;rij < 450;rij += 50) {
-    for (var kolom = 0;kolom < 450;kolom += 50) {
+  for (var rij = 0; rij < 500; rij += 50) {
+    for (var kolom = 0; kolom < 500; kolom += 50) {
+      if ((rij / 50 + kolom / 50) % 2 === 0) {
+        kleur = 'white';
+      } else {
+        kleur = 'black';
+      }
       fill(kleur);
-      rect(kolom,rij,50,50);
+      rect(kolom, rij, 50, 50);
     }
   }
 }
